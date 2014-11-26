5c533ca6482847d873fa5d0c34db5fd7623a13fc
Juan Antonio Cano <jacanosalado@gmail.com>
2014-11-26 01:41:08 +0100

    Moving some files, still not working :(

--- Box2DCSAndroidLib/jni/Android.mk
+++ Box2DCSAndroidLib/jni/Android.mk
@@ -1,23 +1,27 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
+
 SOURCE_BUFFER	:= $(wildcard $(LOCAL_PATH)/Box2D/*.cpp)
-BOX2D_SRC	:= $(subst jni/, , $(SOURCE_BUFFER))
+BOX2D_SRC		:= $(subst jni/, , $(SOURCE_BUFFER))
 SOURCE_BUFFER	:= $(wildcard $(LOCAL_PATH)/Box2D/Collision/*.cpp)
 COLLISION_SRC	:= $(subst jni/, , $(SOURCE_BUFFER))
 SOURCE_BUFFER	:= $(wildcard $(LOCAL_PATH)/Box2D/Collision/Shapes/*.cpp)
-SHAPES_SRC	:= $(subst jni/, , $(SOURCE_BUFFER))
+SHAPES_SRC		:= $(subst jni/, , $(SOURCE_BUFFER))
 SOURCE_BUFFER	:= $(wildcard $(LOCAL_PATH)/Box2D/Common/*.cpp)
-COMMON_SRC	:= $(subst jni/, , $(SOURCE_BUFFER))
+COMMON_SRC		:= $(subst jni/, , $(SOURCE_BUFFER))
 SOURCE_BUFFER	:= $(wildcard $(LOCAL_PATH)/Box2D/Dynamics/*.cpp)
 DYNAMICS_SRC	:= $(subst jni/, , $(SOURCE_BUFFER))
 SOURCE_BUFFER	:= $(wildcard $(LOCAL_PATH)/Box2D/Dynamics/Contacts/*.cpp)
 CONTACTS_SRC	:= $(subst jni/, , $(SOURCE_BUFFER))
 SOURCE_BUFFER	:= $(wildcard $(LOCAL_PATH)/Box2D/Dynamics/Joints/*.cpp)
-JOINTS_SRC	:= $(subst jni/, , $(SOURCE_BUFFER))
+JOINTS_SRC		:= $(subst jni/, , $(SOURCE_BUFFER))
 SOURCE_BUFFER	:= $(wildcard $(LOCAL_PATH)/Box2D/Rope/*.cpp)
-ROPE_SRC	:= $(subst jni/, , $(SOURCE_BUFFER))
-LOCAL_MODULE			:= box2d
-LOCAL_LDLIBS			  := -lstdc++ -lm -landroid
+ROPE_SRC		:= $(subst jni/, , $(SOURCE_BUFFER))
+
+
+LOCAL_MODULE		:= Box2D
+LOCAL_LDLIBS		:= -lstdc++ -lm -landroid
+
 LOCAL_C_INCLUDES		:=	$(LOCAL_PATH)Box2D \
 					$(LOCAL_PATH)Box2D/Collision \
 					$(LOCAL_PATH)Box2D/Collision/Shapes \
@@ -26,6 +30,7 @@ LOCAL_C_INCLUDES		:=	$(LOCAL_PATH)Box2D \
 					$(LOCAL_PATH)Box2D/Dynamics/Contacts \
 					$(LOCAL_PATH)Box2D/Dynamics/Joints \
 					$(LOCAL_PATH)Box2D/Rope
+					
 LOCAL_SRC_FILES			  :=	$(BOX2D_SRC) \
 					$(COLLISION_SRC) \
 					$(SHAPES_SRC) \
@@ -34,8 +39,5 @@ LOCAL_SRC_FILES			  :=	$(BOX2D_SRC) \
 					$(CONTACTS_SRC) \
 					$(JOINTS_SRC) \
 					$(ROPE_SRC)
-include $(BUILD_STATIC_LIBRARY)
-include $(CLEAR_VARS)
-LOCAL_MODULE           := box2dshared
-LOCAL_STATIC_LIBRARIES := box2d
+					
 include $(BUILD_SHARED_LIBRARY)
